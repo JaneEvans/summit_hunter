b4db70a4e75140c1e142c553926a8a9efc87ef3a
Jinjin <jige@expedia.com>
2019-04-21 13:25:09 -0700

    Update Portfolio.js

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -29,7 +29,7 @@ class Portfolio extends Component {
         <div className="row">
           <div className="twelve columns collapsed">
 
-              <h1>Check Out Our Featured Outdoor Articles.</h1>
+              <h1>Check Out Our Featured Outdoor Articles</h1>
 
               <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                   {projects}
